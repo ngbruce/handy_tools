5b63439f80a9467e0f56b1d8d271e40da2bc3940
NgBruce <woobruce@163.com>
2024-03-23 11:46:11 +0800

    update text_finder

--- text_finder.py
+++ text_finder.py
@@ -7,18 +7,20 @@ import io
 # 有一些exe文件则在末尾处有嵌入此字符串
 
 # 设置要搜索的文件夹：
-path_to_search = 'D:\\MY_DOCUMENTS\\Python_Projs\\My_Apps\\handy_tools'
+path_to_search = "E:\\Driver_install\\551.86-desktop-win10-win11-64bit-international-nsd-dch-whql\\Display.Driver"  #
+# 'D:\\MY_DOCUMENTS\\Python_Projs\\My_Apps\\handy_tools'
 # 'D:\\Conda_offline\\new3109cc' # 'C:\\Users\\Bruce\\anaconda3\\envs\\new3109cc'
 
 # 要搜索的字符串（大小写敏感）
-search_string = 'Win_tools'
+search_string = 'Tesla'
 # zx7: 'F:\\Installed_Soft\\Anaconda3'  'Qwen'
 # x99: 'D:\\Conda_offline\\new3109cc' # 'C:\\Users\\Bruce\\anaconda3\\envs\\new3109cc'
 
 # 忽略特定扩展名的文件：
 # .pyc是编译的文件，应该先运行 del_pycache.py 清除，exe文件里面也可能带有文本存放的路径      '.pyc', '.exe', '.dll',
-# ignore_files = ( '.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tiff', '.bin', '.so')
-ignore_files = ('.png', '.jpg', '.jpeg', '.gif', '.bmp')
+# 搜索conda路径要包括无扩展名文件，若要忽略无扩展名文件，加入 ''
+ignore_files = ( '.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tiff', '.bin', '.so', '.exe', '.dll', '.sys', '.mof', '')
+# ignore_files = ('.png', '.jpg', '.jpeg', '.gif', '.bmp')
 
 # 选项，大小写是否敏感
 # case_sensitive = False
@@ -51,7 +53,9 @@ def search_directory_for_string(path, search_string):
     for root, dirs, files in os.walk(path):
         for filename in files:
             # 忽略特定扩展名的文件，只检查可能的文本文件
-            if not filename.endswith(ignore_files):
+            # if not filename.endswith(ignore_files):
+            file_extension = os.path.splitext(filename)[1].lower()
+            if (file_extension not in ignore_files):
                 file_path = os.path.join(root, filename)
                 # 检查文件是否包含搜索字符串
                 check_file_for_string(file_path, search_string)
